11db4877f29b62895f1218306f3f5e1ff7331cfa
kidagine <davidkalatzis1999@hotmail.com>
2020-12-02 01:30:10 +0100

    General setting up and adding tile set with tile rules.

---
+++ Assets/Scripts/ManagerScripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject _player;
+
+    public static GameManager Instance { get; private set; }
+
+
+    void Awake()
+    {
+        CheckInstance();
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        //_player.transform.position = GlobalSettings.PlayerPosition;
+    }
+
+    private void CheckInstance()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public GameObject GetPlayer()
+    {
+        return _player;
+    }
+
+    public int GetSaveSlotIndex()
+    {
+        return 2;
+    }
+
+    public void RestartToCheckpoint()
+    {
+
+    }
+}
