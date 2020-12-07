95c3d2c2778a848d748029572798c606ead211ba
kidagine <davidkalatzis1999@hotmail.com>
2020-12-07 03:38:05 +0100

    Initial commit for setting up the press start scene.

---
+++ Assets/Scripts/EnumScripts/ManagerEnumScripts/Levels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum LevelsEnums { SplashScreen, PressStart, MainMenu, Game, Credits };
+
+public class Levels : MonoBehaviour
+{
+	[SerializeField] private LevelsEnums _levelsEnum = default;
+
+	public LevelsEnums LevelsEnums { get { return _levelsEnum; } private set { } }
+}
---
+++ Assets/Scripts/ManagerScripts/LevelManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+
+public class LevelManager : Singleton<LevelManager>
+{
+    private readonly string _sceneAppendix = "_SCN";
+
+
+    public void GoToLevel(Levels levels)
+    {
+        SceneManager.LoadScene(levels.LevelsEnums + _sceneAppendix);
+    }
+}
