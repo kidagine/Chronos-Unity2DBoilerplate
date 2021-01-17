a1cafa69dca3a0e9bc240843f9e30b292847c97a
kidagine <davidkalatzis1999@hotmail.com>
2021-01-17 20:40:10 +0100

    Added the audio menu and fuctionality.

---
+++ Assets/Scripts/UIScripts/BaseSlider.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class BaseSlider : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Animator _animator = default;
+    [SerializeField] private EntityAudio _entityAudio = default;
+    [SerializeField] private EventSystem _eventSystem = default;
+    [SerializeField] private UnityEvent _onClickedAnimationEnd = default;
+
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        _entityAudio.Play("Selected");
+        _animator.SetBool("IsSelected", true);
+    }
+
+    public void OnDeselect(BaseEventData eventData)
+    {
+        _animator.SetBool("IsSelected", false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _eventSystem.SetSelectedGameObject(gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_eventSystem.currentSelectedGameObject != gameObject)
+        {
+            _eventSystem.SetSelectedGameObject(null);
+        }
+    }
+
+    public void OnValueChange()
+    {
+        _entityAudio.Play("Pressed");
+        //_animator.SetTrigger("Clicked");
+    }
+
+    public void OnClickedEndAnimationEvent()
+    {
+        _onClickedAnimationEnd?.Invoke();
+    }
+}
